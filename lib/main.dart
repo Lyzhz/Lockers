396243e04408a231f50934b8b6bb294ee84776f0
Luyzhz <luisnunes371@gmail.com>
2025-05-19 14:34:10 -0300

    Button Add

--- lib/main.dart
+++ lib/main.dart
@@ -69,9 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+// Bottom NavBar
+  final navigationKey = GlobalKey<CurvedNavigationBarState>();
   int index = 2;
   @override
   Widget build(BuildContext context) {
+    // Botoes da NavBar
     final items = <Widget>[
       Icon(Icons.lock, size: 30),
       Icon(Icons.search, size: 30),
@@ -79,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
       Icon(Icons.settings, size: 30),
       Icon(Icons.person, size: 30),
     ];
-
+    // Base da NavBar
     return Scaffold(
       extendBody: true,
       backgroundColor: Colors.red, 
@@ -88,21 +91,52 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
         centerTitle: true
       ),
-      body: Image.network('https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-5904-622f-b02f-6a26d6d1028b/raw?se=2025-05-19T16%3A52%3A04Z&sp=r&sv=2024-08-04&sr=b&scid=00000000-0000-0000-0000-000000000000&skoid=bbd22fc4-f881-4ea4-b2f3-c12033cf6a8b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-19T15%3A44%3A11Z&ske=2025-05-20T15%3A44%3A11Z&sks=b&skv=2024-08-04&sig=oFosQgXks4EC1%2BiPAnDPIpqkmyM8BuqiavqtLLtiXec%3D',
-      height: double.infinity,
-      width: double.infinity,
-      fit:BoxFit.cover,
-    ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$index',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 120,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                minimumSize: Size(250, 56),
+              ),
+              child: Text('Go to 0', style: TextStyle(fontSize: 32)),
+              onPressed: () {
+                final navigationState = navigationKey.currentState!;
 
-      bottomNavigationBar: CurvedNavigationBar(items: items,
-      animationCurve: Curves.easeInOut,
-      animationDuration: Duration(milliseconds: 300),
-      color: const Color.fromRGBO(40, 86, 155, 1),
-      buttonBackgroundColor: const Color.fromRGBO(47, 180, 242, 1),
-      height: 75,
-      backgroundColor: Colors.transparent,
-      index: index,
-      onTap: (index) => setState(() => this.index = index),
+                navigationState.setPage(0);
+              },
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          iconTheme: IconThemeData(color: const Color.fromRGBO(40, 86, 155, 1)),
+        ),
+        child: CurvedNavigationBar(
+          key: navigationKey,
+          items: items,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          color: const Color.fromRGBO(47, 180, 242, 1),
+          buttonBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          height: 75,
+          backgroundColor: Colors.transparent,
+          index: index,
+          onTap: (index) => setState(() => this.index = index),
+          
+        ),
       ),
     );
   }
